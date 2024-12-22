56b68c323d63d33006d4ee47b7a314575bfe7fe8
hyejux <hyejux@gmail.com>
2024-12-23 01:17:31 +0900

    Feat: Add Marker, Info - Map Marker 띄우기 (여러개 / 고정값) - Marker 선택 시 Info 창 띄우기

--- src/pages/Map/MapAPI.jsx
+++ src/pages/Map/MapAPI.jsx
@@ -26,10 +26,21 @@ function MapAPI() {
       const { naver } = window;
 
       if (naver) {
+
+        // const HOME_PATH = window.HOME_PATH || ".";
+        const position = new naver.maps.LatLng(location.latitude, location.longitude);
+  
+        // // 네이버 지도 객체 생성
+        // mapRef.current = new naver.maps.Map(mapContainer.current, {
+        //   center: position,
+        //   zoom: 19,
+        // });
+
+
         const mapOptions = {
           center: new naver.maps.LatLng(location.latitude, location.longitude), // 내 위치
           zoom: 10,
-          minZoom: 15,
+          minZoom: 16,
           useStyleMap: false,
           mapTypeControl: false,
           mapTypeControlOptions: {
@@ -45,14 +56,91 @@ function MapAPI() {
         // 네이버 지도 객체 생성
         mapRef.current = new naver.maps.Map(mapContainer.current, mapOptions);
 
-        // 마커 추가
-        const marker = new naver.maps.Marker({
-          position: new naver.maps.LatLng(location.latitude, location.longitude),
+        // 마커 옵션 1
+        const markerOptions = {
+          position: position.destinationPoint(90, 0), // 90도 방향으로 15m 떨어진 위치
           map: mapRef.current,
-        });
+          icon: {
+            url:`public/image2.png`,  //50픽셀
+            size: new naver.maps.Size(100, 100),
+            origin: new naver.maps.Point(0, 0),
+            // anchor: new naver.maps.Point(25, 26),
+          },
+        };
 
-        // 정보창 추가
-        const contentString = '<div>여기에 정보창 내용을 넣으세요!</div>';
+        // 마커 옵션 2
+        const markerOptions2 = {
+          position: position.destinationPoint(10, 300), // 90도 방향으로 15m 떨어진 위치
+          map: mapRef.current,
+          icon: {
+            url:`public/image3.png`,  //50픽셀
+            size: new naver.maps.Size(100, 100),
+            origin: new naver.maps.Point(0, 0),
+            // anchor: new naver.maps.Point(25, 26),
+          },
+        };
+
+        // 마커 옵션
+        const markerOptions3 = {
+          position: position.destinationPoint(40, -200), // 90도 방향으로 15m 떨어진 위치
+          map: mapRef.current,
+          icon: {
+            url:`public/image4.png`,  //50픽셀
+            size: new naver.maps.Size(100, 100),
+            origin: new naver.maps.Point(0, 0),
+            // anchor: new naver.maps.Point(25, 26),
+          },
+        };
+        
+
+        // 마커 추가 (1)
+        const marker =  new naver.maps.Marker(markerOptions);
+
+        // 마커 추가 (2)
+        const marker2 =  new naver.maps.Marker(markerOptions2);
+        
+        // 마커 추가 (3)
+        const marker3 =  new naver.maps.Marker(markerOptions3);
+      
+
+        // new naver.maps.Marker({
+        //   position: new naver.maps.LatLng(location.latitude, location.longitude),
+        //   map: mapRef.current,
+        // });
+
+
+        // 정보창 추가 (1)
+        const contentString = [
+          '<div style="display: flex; gap: 5px; width: 350px; padding: 10px; border: 1px solid black; border-radius: 5px;">',
+          '  <div style="position: relative;">',
+          '    <div style="margin: 10px; border: 1px solid black; border-radius: 100%; width: 100px; height: 100px;">',
+          '      <img src="public/img_dog.png" style="width: 100%; height: 100%; object-fit: cover;" />',
+          '    </div>',
+          '    <div style="position: absolute; top: 65px; left: 65px; border: 1px solid black; border-radius: 100%; background-color: #ddd; width: 50px; height: 50px;">',
+          '      활성화',
+          '    </div>',
+          '    <div>',
+          '      <button style="width: 120px; border: 1px solid blue; border-radius: 5px;">버튼</button>',
+          '    </div>',
+          '  </div>',
+          '  <div style="width: 100%; display: flex; flex-direction: column;">',
+          '    <div style="height: 80%;">',
+          '      <div style="font-size: 20px; font-weight: bold; padding: 5px;">웰시코기 123</div>',
+          '      <div style="padding: 5px;">소개 글이 없습니다.</div>',
+          '    </div>',
+          '    <div style="height: 20%;">',
+          '      <div style="float: right; display: flex; gap: 5px;">',
+          '        <div style="border: 1px solid blue; border-radius: 5px; padding: 3px 5px;">이름</div>',
+          '        <div style="border: 1px solid blue; border-radius: 5px; padding: 3px 5px;">이름</div>',
+          '        <div style="border: 1px solid blue; border-radius: 5px; padding: 3px 5px;">이름</div>',
+          '        <div style="border: 1px solid blue; border-radius: 5px; padding: 3px 5px;">...</div>',
+          '      </div>',
+          '    </div>',
+          '  </div>',
+          '</div>',
+        ].join('');
+        
+
         const infowindow = new naver.maps.InfoWindow({
           content: contentString,
           anchorSize: new naver.maps.Size(15, 5),
@@ -68,9 +156,13 @@ function MapAPI() {
           }
         });
 
-        // 정보창 처음에 열기
-        infowindow.open(mapRef.current, marker);
+        // // 정보창 처음에 열기
+        // infowindow.open(mapRef.current, marker);
       }
+
+
+
+
     }, [location]);
     
     // 내 위치로 가기
@@ -86,27 +178,6 @@ function MapAPI() {
     };
   
     
-    // var HOME_PATH = window.HOME_PATH || '.';
-    // var position = new naver.maps.LatLng(37.3849483, 127.1229117);
-
-    // var map = new naver.maps.Map('map', {
-    //     center: position,
-    //     zoom: 19
-    // });
-
-    // var markerOptions = {
-    //     position: position.destinationPoint(90, 15),
-    //     map: map,
-    //     icon: {
-    //         url: HOME_PATH +'/img/example/sally.png',
-    //         size: new naver.maps.Size(50, 52),
-    //         origin: new naver.maps.Point(0, 0),
-    //         anchor: new naver.maps.Point(25, 26)
-    //     }
-    // };
-
-    // var marker = new naver.maps.Marker(markerOptions);
-      
 
   return (
     <>
